99b013cb6829aef1d62c2ee9c91563f98acb3b86
Mia-Salazar <maria.sgr@gmail.com>
2022-08-07 18:16:24 +0200

    Add useReducer to add to favourites

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -1,9 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import "./styles.css";
 
+const initialState = {
+    favorites: []
+}
+  
+const favoriteReducer = (state, action) => {
+    switch (action.type) {
+      case 'ADD_TO_FAVORITE':
+        return {
+          ...state,
+          favorites: [...state.favorites, action.payload]
+        };
+      default:
+        return state;
+    }
+}
+
 const CharacterList = () => {
     const [list, setList] = useState([]);
+    const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+
 
     useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character')
@@ -11,19 +29,38 @@ const CharacterList = () => {
             .then(data => setList(data.results))
     }, []);
 
+    const handleClick = favorite => {
+        dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
+    }
+
     return (
-        <section class="list">
-            {list.map(character => (
-                <article class="list-element">
-                    <figure>
-                        <img src={character.image} alt=""/>
-                    </figure>
-                    <h2>{character.name}</h2>
-                    <p>Número de episodios: {character.episode.length}</p>
-                    <p>Origen: {character.origin.name}</p>
-                    <p>Especie: {character.species}</p>
-                </article>
-            ))}
+        <section class="container">
+            <h2>Personajes favoritos</h2>
+            <ul className="list-favourites">
+                {favorites.favorites.map(favorite => (
+                    <li key={favorite.id}>
+                    {favorite.name}
+                    </li>
+                ))}
+            </ul>
+            {favorites.favorites.length === 0 &&  <h4>No hay favoritos</h4>}
+
+            <h2>Personajes de la serie</h2>
+            <div className="list-characters">
+                {list.map(character => (
+                    <article class="list-element" key={character.id}>
+                        <figure>
+                            <img src={character.image} alt=""/>
+                        </figure>
+                        <h3>{character.name}</h3>
+                        <p>Número de episodios: {character.episode.length}</p>
+                        <p>Origen: {character.origin.name}</p>
+                        <p>Especie: {character.species}</p>
+                        <button type="button" onClick={() => handleClick(character)}>Agregar a Favoritos</button>
+                    </article>
+                ))}
+            </div>
+
         </section>
     )
 };
